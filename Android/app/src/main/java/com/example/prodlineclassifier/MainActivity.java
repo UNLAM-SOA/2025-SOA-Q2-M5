b76c0c7f6bb9b5254b64b2471e974e2dd21a1510
Nahuel Alberto Rivas <nahuelrivas41@gmail.com>
2025-10-21 22:10:49 -0300

    Implementación de activities Settings y ProcessLog, más funcionalidades básicas para visualización de datos e interacción con el embebido

--- Android/app/src/main/java/com/example/prodlineclassifier/MainActivity.java
+++ Android/app/src/main/java/com/example/prodlineclassifier/MainActivity.java
@@ -1,13 +1,15 @@
 package com.example.prodlineclassifier;
 
-import android.annotation.SuppressLint;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,13 +17,15 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import java.util.Objects;
-
 public class MainActivity extends AppCompatActivity {
 
-    public Button startMain;
-    public Button stopMain;
-    public Button viewLog;
+    public Button btnSettingsMain;
+    public Button btnStartMain;
+    public Button btnStopMain;
+    public Button btnProcessLogMain;
+    public TextView txtViewSystemStatus;
+    public String systemStatus;
+
     public MainBroadcastReceiver mainBroadcastReceiver;
 
     @Override
@@ -30,7 +34,37 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
+        systemStatus = getString(R.string.txt_view_status);
 
+        txtViewSystemStatus = findViewById(R.id.txt_view_systemstatus_main);
+
+        btnSettingsMain = findViewById(R.id.btn_settings_main);
+
+        btnSettingsMain.setOnClickListener(v -> {
+            // Intent para abrir Settings.java
+            Intent intent = new Intent(MainActivity.this, Settings.class);
+            startActivity(intent);
+        });
+
+        btnProcessLogMain = findViewById(R.id.btn_process_log_main);
+
+        btnProcessLogMain.setOnClickListener(v -> {
+            // Intent para abrir Settings.java
+            Intent intent = new Intent(MainActivity.this, ProcessLog.class);
+            startActivity(intent);
+        });
+
+        btnStartMain = findViewById(R.id.btn_start_main);
+
+        btnStartMain.setOnClickListener(v -> {
+            updateSysStatus(getString(R.string.info_sysstat_start_main));
+        });
+
+        btnStopMain = findViewById(R.id.btn_stop_main);
+
+        btnStopMain.setOnClickListener(v -> {
+            updateSysStatus(getString(R.string.info_sysstat_stop_main));
+        });
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
@@ -39,13 +73,36 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    View.OnClickListener onClickListener = new View.OnClickListener() {
-        @SuppressLint("SetTextI18N")
-        @Override
-        public void onClick(View v) {
+    private void updateSysStatus(String newSystemStatus) {
+        txtViewSystemStatus = findViewById(R.id.txt_view_systemstatus_main);
 
+        GradientDrawable bgDrawable = (GradientDrawable) txtViewSystemStatus.getBackground();
+        int color;
+
+        switch (newSystemStatus) {
+            case "Running":
+                if(systemStatus.equals("Running")) {
+                    Toast.makeText(this, "System is already running", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                color = Color.parseColor(getString(R.string.color_sysstat_running));
+                break;
+            case "Manually Stopped":
+                if(!systemStatus.equals("Running")) {
+                    Toast.makeText(this, "System isn't running right now", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                color = Color.parseColor(getString(R.string.color_sysstat_manually_stopped));
+                break;
+            default:
+                color = Color.parseColor(getString(R.string.color_sysstat_undefined));
+                break;
         }
-    };
+
+        txtViewSystemStatus.setText(newSystemStatus);
+        systemStatus = newSystemStatus;
+        bgDrawable.setColor(color);
+    }
 
     public static class MainBroadcastReceiver extends BroadcastReceiver {
 
--- Android/app/src/main/java/com/example/prodlineclassifier/MainService.java
+++
@@ -1,4 +0,0 @@
-package com.example.prodlineclassifier;
-
-public class MainService {
-}
